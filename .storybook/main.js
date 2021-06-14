ae5fb5fe87424b976e0e6a28429426e5f19767c5
Pavittar Singh <singh.pavittar95@icloud.com>
2021-06-14 23:00:15 +0530

    storybook added

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  stories: [
+    "../pages/**/*.stories.tsx",
+    "../components/**/*.stories.tsx",
+    "../pages/**/*.stories.js",
+    "../components/**/*.stories.js",
+  ],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-docs",
+    "@storybook/addon-controls",
+    "@storybook/addon-viewport",
+  ],
+};
---
+++ components/common/button.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+import { Button } from "./button.stories";
+
+export const Primary = () => <Button primary>Button</Button>;
